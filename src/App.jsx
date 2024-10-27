f58d4dc453954f10bbd5a1ecf801939c36294717
fedexid <142997423+fedexid@users.noreply.github.com>
2024-10-27 11:16:41 +0700

    feat: filter select tasks

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 function App() {
   const [tasks, setTasks] = useState([]);
   const [values, setValues] = useState("");
+  const [filter, setFilter] = useState("All");
 
   useEffect(() => {
     fetchTodos();
@@ -25,26 +26,31 @@ function App() {
     let addTasks;
 
     addTasks = {
-      id: Date.now(),
       title: values,
       completed: false,
     };
 
-    values && setTasks([...tasks, addTasks]);
+    values && setTasks((t) => [...t, addTasks]);
     setValues("");
   };
 
   const handleDelete = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
+    setTasks((t) => t.filter((task) => task.id !== id));
   };
 
   const handleCompleted = (id) => {
-    setTasks(
-      tasks.map((task) =>
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
         task.id === id ? { ...task, completed: !task.completed } : task,
       ),
     );
   };
+
+  const filteredLists = tasks.filter((task) => {
+    if (filter === "All") return true;
+    if (filter === "Completed") return task.completed === true;
+    if (filter === "Not-Completed") return task.completed === false;
+  });
 
   return (
     <div className="container mx-auto h-screen rounded bg-slate-800 lg:my-6 lg:h-max lg:w-3/5">
@@ -52,24 +58,39 @@ function App() {
         <h1 className="text-4xl font-bold text-white lg:text-6xl">
           Todo Lists
         </h1>
-        <input
-          type="text"
-          className="my-4 h-8 w-3/4 max-w-80 rounded"
-          onChange={(e) => setValues(e.target.value)}
-          value={values}
-          placeholder="Enter Tasks"
-        />
-        <button
-          onClick={addTodos}
-          type="submit"
-          className="rounded bg-slate-400 px-4 py-2 font-bold drop-shadow-md transition ease-in-out hover:bg-slate-600"
-        >
-          Add Tasks
-        </button>
 
-        {tasks.map((task) => (
+        <div className="my-4 flex w-64 items-center space-x-2 lg:w-80">
+          <input
+            type="text"
+            className="h-8 w-3/4 rounded"
+            onChange={(e) => setValues(e.target.value)}
+            value={values}
+            placeholder="Enter Tasks"
+          />
+          <button
+            onClick={addTodos}
+            type="submit"
+            className="h-8 w-1/4 rounded bg-slate-400 font-bold drop-shadow-md transition ease-in-out hover:bg-slate-600 lg:min-w-24"
+          >
+            <span className="block lg:hidden">➕</span>
+            <span className="hidden lg:block">Add Tasks</span>
+          </button>
+        </div>
+
+        <select
+          className="mb-4 w-64 rounded p-2 lg:w-80"
+          name="taskFilter"
+          id="taskFilter"
+          onChange={(e) => setFilter(e.target.value)}
+        >
+          <option value="All">All</option>
+          <option value="Completed">Completed</option>
+          <option value="Not-Completed">Not Completed</option>
+        </select>
+
+        {filteredLists.map((task) => (
           <div
-            className="my-2 flex w-3/5 items-center justify-between gap-2 text-white md:w-3/4 lg:w-1/2"
+            className="my-2 flex w-3/4 items-center justify-between gap-2 text-justify text-white lg:w-1/2"
             key={task.id}
           >
             <h2
